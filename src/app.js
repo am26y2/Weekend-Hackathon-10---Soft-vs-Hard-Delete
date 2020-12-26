38aef0bfc7f088cc9926bfd83a3ec25ffe71535d
Amit Raj <amitraj9046@gmail.com>
2020-12-26 14:56:43 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,59 +1,54 @@
-const express = require("express");
-const Student = require("./models/Student");
+const express = require('express');
+const Student = require('./models/Student');
+
 
 const app = express();
 
-// middleware
+// middleware 
 app.use(express.json());
 
 // Routes
 
 // Get all the students
-app.get("/students", async (req, res) => {
-  // write your codes here
-  res.send(JSON.stringify(Student));
-});
+app.get('/students', async (req, res) => {
+    res.send(await Student.find({isDeleted:false}))
+})
 
 // Add student to database
-app.post("/students", async (req, res) => {
-  // write your codes here
-  let name = req.body.name;
-  let age = req.body.age;
-  let Class = req.body.class;
-  let sex = req.body.sex;
-  let grade_point = req.body.grade_point;
-  if (!name && !age && !Class && !sex && !grade_point) {
-    res.status(404).send("Error");
-    return;
-  }
-  let obj = { name, age, Class, sex, grade_point };
-  Student.push(JSON.stringify(obj);
-});
+app.post('/students', async (req, res) =>{
+    const body=req.body;
+    const newStudent=Student(body)
+    res.send(await newStudent.save())
+    
+})
 
 // Get specific student
-app.get("/students/:id", async (req, res) => {
-  // write your codes here
-  let id = req.params.id;
-  let student = Student.find((el) => el.id === parseInt(id));
-  if (!student) {
-    res.status(404).send("Error");
-    return;
-  }
-  res.send(JSON.stringify(student));
-});
+app.get('/students/:id', async (req, res) =>{
+    try{
+        const  givenId=await Student.findOne({
+            _id:req.params.id,
+            isDeleted:false
+        })
+        if(givenId!=null && givenId!=undefined){
+            res.send(givenId);
+        }else{
+            res.sendStatus(404)
+        }
+    }catch(er){
+        res.sendStatus(404);
+
+    }
+})
 
 // delete specific student
-app.delete("/students/:id", async (req, res) => {
-  // write your codes here
-  let id = req.params.id;
-  let student = Student.find((el) => el.id === parseInt(id));
-  if (!student) {
-    res.status(404).send("Error");
-    return;
-  }
-  let copy = { ...Student };
-  copy.remove(student);
-  res.send(JSON.stringify(copy));
-});
+app.delete('/students/:id', async (req, res) =>{
+    if(req.query.type.toLowerCase()==="soft"){
+        await Student.updateOne({_id:req.params.id},{isDeleted:true})
+    }else if(req.query.type.toLocaleLowerCase()==="hard"){
+        await Student.deleteOne({_id:req.params.id})
+    }
+    res.sendStatus(200);
+}) 
+
 
 module.exports = app;
